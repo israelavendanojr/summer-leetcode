9c84515df5557ec6568d0121078f57d338aa0926
israelavendanojr <iajr0207@gmail.com>
2025-07-02 12:22:22 -0700

    Review container_with_most_water.py

--- TwoPointer/container_with_most_water.py
+++ TwoPointer/container_with_most_water.py
@@ -15,3 +15,23 @@ class Solution:
             max_a = max(a, max_a)
         
         return max_a
+    
+# Review Attempt
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        l, r = 0, len(height) - 1
+        max_a = 0
+
+        while l < r:
+            h = min(height[l], height[r])
+            a = h * (r-l)
+
+            if height[l] == h:
+                l += 1
+            else:
+                r -= 1
+            
+            max_a = max(max_a, a)
+
+        return max_a
+
